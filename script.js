b2418238224ea25e8cf949fcae91d7da9a545f07
romipro-crack <rominauwu892@gmail.com>
2026-05-12 21:22:06 -0300

    desafio1

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 fetch("https://rickandmortyapi.com/api/character")
-.then(respuesta => respuesta.json())
-.then(datos => {
-datos.results.forEach(personaje => {
-document.getElementById("personajes").innerHTML += `
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        datos.results.forEach(personaje => {
+            document.getElementById("personajes").innerHTML += `
             <div class="col-md-3 mb-4">
                 <div class="card">
                     <img src="${personaje.image}" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">
                             ${personaje.name}
+                        
+                            
+                            <p class="card-subtitle text-muted">
+                             ${personaje.species}
+                            </p>
+
+                            <p class="card-subtitle text-muted">
+                             ${personaje.status}
+                            </p>
+
                         </h5>
-                        <p>
-                            ${personaje.species}
-                        </p>
                     </div>
                 </div>
             </div>
@@ -20,3 +27,4 @@ document.getElementById("personajes").innerHTML += `
             `;
         });
     });
+
